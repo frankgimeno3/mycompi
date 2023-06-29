e10af6cc52190bbb95f0fdebfdf9707701ad9bf6
Frank Gimeno <frankgimeno3@gmail.com>
2023-06-29 13:09:39 +0200

    le meto estructura al mainroller

--- components/dashboard/Mainroller.tsx
+++ components/dashboard/Mainroller.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 const Mainroller: React.FC = () => {
   return (
-    <div >
-
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <button
+        style={{
+          backgroundColor: 'white',
+          borderRadius: '100%',
+          boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.25)',
+          color: 'black',
+          padding: '10px 20px',
+          fontSize: '16px',
+          fontWeight: 'bold'
+        }}
+      >
+        Mi pisito
+      </button>
     </div>
   );
 };
