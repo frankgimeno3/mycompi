ebd3d38a10a0deef24925ba6e2aa570744112920
Frank Gimeno <frankgimeno3@gmail.com>
2023-06-29 13:51:20 +0200

    añado servicestab

--- components/dashboard/Servicestab.tsx
+++ components/dashboard/Servicestab.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 const Servicestab: React.FC = () => {
   return (
-    <>
-    </>
+    <nav className="bg-yellow-400 py-3">
+      <div className="container mx-auto px-4 text-center  font-bold">
+        <h2 className="text-lg text-purple-900 mx-7">
+          Contrata servicios para tu pisito
+        </h2>
+      </div>
+      <h3 className="text-md text-purple-900 ml-10 pl-5 font-bold mt-10">Mudanzas y transportes</h3>
+      <h3 className="text-md text-purple-900 ml-10 pl-5 font-bold mt-10">Instalación y montaje</h3>
+      <h3 className="text-md text-purple-900 ml-10 pl-5 font-bold mt-10">Fontanería</h3>
+      <h3 className="text-md text-purple-900 ml-10 pl-5 font-bold mt-10">Electricidad</h3>
+      <h3 className="text-md text-purple-900 ml-10 pl-5 font-bold mt-10">Pintura</h3>
+      <h3 className="text-md text-purple-900 ml-10 pl-5 font-bold mt-10">Servicios de Limpieza</h3>
+      <h3 className="text-md text-purple-900 ml-10 pl-5 font-bold mt-10">Control de plagas</h3>
+    </nav>
   );
 };
 
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import Mainroller from '../../components/dashboard/Mainroller';
 import Servicestab from '../../components/dashboard/Servicestab';
 
-
 const Dashgboard: React.FC = () => {
   return (
-    <div className="min-h-screen flex items-start justify-center bg-gradient-to-br from-purple-500 to-purple-900">
-      <div className="text-center mt-5 p-10">
-        <h1 className="text-5xl font-bold text-yellow-400 mt-20">MyCompi</h1>
-        <h2 className="text-white text-xl  mt-5">Selecciona una opción para continuar </h2>
-      <Mainroller/>
-      <Servicestab/>
+    <>
+      <div className=" flex items-start justify-center bg-gradient-to-br from-purple-500 to-purple-900 pb-4">
+        <div className="text-center mt-5 p-10 h-90vh mb-20 ">
+          <h1 className="text-5xl font-bold text-yellow-400 mt-20">MyCompi</h1>
+          <h2 className="text-white text-xl mt-5">Selecciona una herramienta para continuar</h2>
+          <Mainroller />
+        </div>
       </div>
-
-    </div>
+      <Servicestab />
+    </>
   );
 };
 
