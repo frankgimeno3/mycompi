3f79f832a935962248431250b261dcfa0f8990bf
Frank Gimeno <frankgimeno3@gmail.com>
2023-06-29 13:00:56 +0200

    mejoro navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-yellow-400 py-4 px-6 flex items-center justify-between">
-      <h1 className="text-lg text-white font-bold">MyCompi</h1>
+    <nav className="fixed top-0 w-full  py-4  flex items-center justify-between">
+      <button className="text-lg text-white font-bold px-6 text-yellow-300">MyCompi</button>
       <div>
-        <button className="mr-2 bg-transparent text-white text-sm">ENG</button>
-        <button className="mr-2 rounded-full bg-white text-sm px-4 py-2 shadow">Log In</button>
-        <button className="rounded-full bg-white text-sm px-4 py-2 shadow">Signup</button>
+        <button className="mr-2 bg-transparent text-yellow-300 font-bold text-sm">ENG</button>
+        <button className="mr-1 rounded-full bg-white text-sm px-4 py-1 shadow">Log In</button>
+        <button className="rounded-full bg-white text-sm px-4 mr-2 py-1 shadow">Signup</button>
       </div>
     </nav>
   );
