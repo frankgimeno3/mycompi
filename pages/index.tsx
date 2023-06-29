b4798d3f7d8683ef132c4878a601e1d01ce1efd0
Frank Gimeno <frankgimeno3@gmail.com>
2023-06-29 12:45:04 +0200

    añado estilos landing

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,11 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex bg-purple-500">
-      <h1 className="text-3xl text-bold">MyCompi</h1>
-      <h2 className="text-white text-xl">Todas las herramientas para compartir piso, en una sola app</h2>
+    <div className="min-h-screen flex items-start justify-center bg-gradient-to-br from-purple-500 to-purple-900">
+      <div className="text-center mt-5 p-10">
+        <h1 className="text-5xl font-bold text-yellow-400 mt-20">MyCompi</h1>
+        <h2 className="text-white text-xl  mt-5">Todas las herramientas para compartir piso, <span className='font-bold text-yellow-400'>en una sola app</span></h2>
+      </div>
     </div>
   )
 }
