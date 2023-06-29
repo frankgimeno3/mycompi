a8dac4d3a7664d822fb48b19fa5efc865a5b2eb2
Frank Gimeno <frankgimeno3@gmail.com>
2023-06-29 12:55:15 +0200

    añado navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import Head from 'next/head'
+import Navbar from '../components/navbar'
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+    <title>Empleotecnic.com</title>
+    <link rel="icon" href="/icons/minimal.png" />
+  </Head>
+  <Navbar/>
+  <Component {...pageProps} />
+  </>
+    )
 }
